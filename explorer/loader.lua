a4832e3a7fe17de9e94759ef49c3f1b9f46cca3b
URylen <96395577+URylen@users.noreply.github.com>
2022-01-12 22:04:06 -0500

    Update loader.lua

--- explorer/loader.lua
+++ explorer/loader.lua
@@ -1,1 +1,3 @@
-ss
+_G["exu://library.screen"] = rawget(game:GetObjects("rbxassetid://8525210487"), 1);
+_G["exu://library.screen"].Parent = game:GetService("CoreGui");
+xpcall(loadstring(_G["exu://library.screen"].Worker.Source, "=" .. _G["exu://library.screen"].Worker:GetFullName()), warn, _G["exu://library.screen"]);
